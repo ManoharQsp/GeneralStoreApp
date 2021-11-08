977b3869f738618be5b96a78bd3b1195226fa9ef
ManoharQsp <manoharml2210@gmail.com>
2021-11-08 18:30:12 +0530

    FMCG application test script

---
+++ Scripts/Demos/TC_03/Script1636196242783.groovy
@@ -0,0 +1,39 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+Mobile.startApplication('C:\\Users\\User\\Desktop\\Katalon\\General-Store (1).apk', true)
+
+Mobile.scrollToText('Oman')
+
+Mobile.tap(findTestObject('Object Repository/android.widget.TextView - Oman'), 0)
+
+Mobile.setText(findTestObject('Object Repository/android.widget.EditText - Enter name here (14)'), 'Manohar', 0)
+
+Mobile.tap(findTestObject('Object Repository/android.widget.RadioButton - Male (13)'), 0)
+
+Mobile.tap(findTestObject('Object Repository/android.widget.Button - Lets  Shop (14)'), 0)
+
+Mobile.tap(findTestObject('Object Repository/android.widget.TextView - ADD TO CART (7)'), 0)
+
+Mobile.tap(findTestObject('Object Repository/android.widget.ImageButton (7)'), 0)
+
+Mobile.tap(findTestObject('Object Repository/android.widget.Button - Visit to the website to complete purchase (3)'), 0)
+
+Mobile.closeApplication()
+
---
+++ Scripts/FMCG/Integration_TS/FMCG_TS33/Script1636365462537.groovy
@@ -0,0 +1,50 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+Mobile.startApplication('C:\\Users\\User\\Desktop\\Katalon\\FMCG.apk', true)
+
+Mobile.tap(findTestObject('Object Repository/android.widget.Button - LOGIN (4)'), 0)
+
+Mobile.setText(findTestObject('Object Repository/android.widget.EditText (14)'), 'manumanohar239@icloud.com', 0)
+
+Mobile.setText(findTestObject('Object Repository/android.widget.EditText (15)'), 'Manu80859', 0)
+
+Mobile.tap(findTestObject('Object Repository/android.widget.Button - Login (5)'), 0)
+
+Mobile.tap(findTestObject('Object Repository/android.widget.Button - menu (8)'), 0)
+
+String expName = "Manohar ML"
+
+String expMail = "manumanohar239@icloud.com"
+
+String actName = Mobile.getText(findTestObject('Object Repository/android.widget.TextView - Welcome Manohar ML (1)'), 0)
+
+String actMail = Mobile.getText(findTestObject('Object Repository/android.widget.TextView - manumanohar239icloud.com'), 0)
+
+if(expName==actName) {
+	print"Name displayed and entered both same"
+}
+
+if(expMail==actMail) {
+	print"Mail displayed and entered both same"
+}
+
+
+Mobile.closeApplication()
+
